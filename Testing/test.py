626624a49708d6c1e813a84423861a96d4ba10b2
Sebastian Salguero <bryan.salguero@edu.uah.es>
2021-02-25 13:09:13 +0100

    Testing

---
+++ Testing/test.py
@@ -0,0 +1,12 @@
+print("Hello world!")
+print("It was a dark and stormy night.")
+print('Suddenly a shot rang out!')
+print("Hi! This is a double quote mark: \" We use it for printing.")
+print("Audrey Hepburn once said \"Nothing is impossible. The word itself says 'I'm Possible!'.\"")
+print("This\nis\nmy\nsample.")
+print("This \n is \n my \n sample.")
+print("""You can print
+on multiple
+lines using
+triple
+quotes.""")
